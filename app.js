d283060bad2d57830d61ad6e339bb3143945a016
Jônatas <jonatasthome04@gmail.com>
2025-06-19 17:33:59 -0300

    Started express and handlebars config

--- app.js
+++ app.js
@@ -4,5 +4,16 @@ const app = express()
 const exphbs = require("express-handlebars")
 require ("dotenv").config();
 
+app.use(express.static("public"))
+
+app.engine("handlebars", exphbs.engine())
+app.set("view engine","handlebars")
 
 
+app.get("/", (req,res)=>{
+    res.render("home")
+})
+
+app.listen(3000, ()=>{
+    console.log("Servidor rodando!")
+})
