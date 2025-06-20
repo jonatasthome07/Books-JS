360f1cef63a01f079d420011184caee181fb1459
Jônatas <jonatasthome04@gmail.com>
2025-06-19 21:51:01 -0300

    Modified the form and CSS and added all the middlewares

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const app = express()
 const exphbs = require("express-handlebars")
 require ("dotenv").config();
 
-app.use(express.static("public"))
-
+//Set Engine
 app.engine("handlebars", exphbs.engine())
 app.set("view engine","handlebars")
 
+
+//Middlewares
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+app.use(express.static("public"))
+
+
+//Routes
 app.post("/allbooks", (req,res)=>{
     
     res.render("allbooks")
